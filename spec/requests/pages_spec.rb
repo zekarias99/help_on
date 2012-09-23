501e0429b2c6023e9af37df492fd351ddc22020b
Zekarias Araya <zekarias99@gmail.com>
2012-09-23 13:52:52 -0700

    Modify new syntax

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -6,22 +6,27 @@ describe "Pages" do
 	  
     it "should have the h1 'Helpo On'" do
 
-	   visit '/pages/home'
-	   page.should have_selector('h1', :text => "Helpo On")
-	end
+	    visit '/pages/home'
+	    page.should have_selector('h1', :text => "Helpo On")
+	  end
 
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/pages/home'
-      page.should have_selector('title',:text => "Helpo On | Home")
+      page.should have_selector('title',:text => "Helpo On")
     end 
+
+    it "should not have a custom page title" do
+      visit '/pages/home'
+      page.should_not have_selector('title', :text => " | Home")       
+    end
   end 
  
   describe "About Us page" do 
 	  
     it "should have the h1 'About Us'" do
 
-	   visit '/pages/about'
-	   page.should have_selector('h1', :text => "About Us")
+	    visit '/pages/about'
+	    page.should have_selector('h1', :text => "About Us")
 	end
 
     it "should have the right title" do
@@ -30,27 +35,26 @@ describe "Pages" do
     end 
   end 
  
-  describe " How It Work's page" do 
+  describe " How It Works page" do 
 	  
-    it "should have the h1 'How It Work's'" do
+    it "should have the h1 'How It Works'" do
 
-	   visit '/pages/how'
-	   page.should have_selector('h1', :text => "How It Work\'s")
+	    visit '/pages/how'
+	    page.should have_selector('h1', :text => "How It Works")
 	end
 
     it "should have the right title" do
       visit '/pages/how'
-      page.should have_selector('title',:text => "Helpo On | How It Work\'s")
+      page.should have_selector('title',:text => "Helpo On | How It Works")
     end 
   end 
  
   describe " Contact page" do 
 	  
     it "should have the h1 'Contact" do
-
-	   visit '/pages/contact'
-	   page.should have_selector('h1', :text => "Contact")
-	end
+      visit '/pages/contact'
+	    page.should have_selector('h1', :text => "Contact")
+	  end
 
     it "should have the right title" do
       visit '/pages/contact'
