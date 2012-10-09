9bd7e9042419cf643ecd307981912cb14eacbe37
Zekarias Araya <zekarias99@gmail.com>
2012-10-09 11:28:35 -0700

    Dibug the sign in link

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,23 +1,30 @@
 module SessionsHelper
 	
 	def sign_in(user)
+      #puts "Sign In called"		
 	  cookies.permanent[:remember_token] = user.remember_token
 	  self.current_user = user
 	end
 
 	def signed_in?
+	  #puts "Current User #{current_user}"	
 	  !current_user.nil?	
 	end
 
 	def current_user=(user)
+	  #puts "Setting current user as #{current_user}"	
 	  @current_user = user	
 	end
 
 	def current_user
+	  #puts "Sus User [#{cookies[:remember_token]}]"
+      #sus_user = User.find_by_remember_token(cookies[:remember_token])
+      #puts "Sus User #{sus_user.name}"	
 	  @current_user ||= User.find_by_remember_token(cookies[:remember_token])		
 	end
 
     def sign_out
+    #puts "Sign out called"
       self.current_user = nil
       cookies.delete(:remember_token)	
     end
